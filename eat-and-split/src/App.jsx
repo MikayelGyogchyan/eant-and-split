2f3b6b5bf83bb66423b783a68a4bc089b4e505ce
DavitGyogchyan <mikayel1999@mail.com>
2023-08-03 17:24:46 +0000

    second commit, display and hide the form

--- eat-and-split/src/App.jsx
+++ eat-and-split/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 const initialFriends = [
@@ -21,13 +22,24 @@ const initialFriends = [
   },
 ];
 
+function Button({children, onClick}) {
+  return (
+    <button className="button" onClick={onClick}>{children}</button>
+  )
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false)
+  
+  function handleShowAddFriend() {
+    setShowAddFriend(show => !show)
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend/>
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend/>}
+        <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add friend"}</Button>
       </div>
 
       <FormSplitBill/>
@@ -68,12 +80,6 @@ function Friend({ friend }) {
       <Button>Select</Button>
     </li>
   );
-}
-
-function Button({children}) {
-  return (
-    <button className="button">{children}</button>
-  )
 }
 
 function FormAddFriend() {
